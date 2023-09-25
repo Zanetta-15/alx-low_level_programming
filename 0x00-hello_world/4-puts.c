83452cfc84223add019d2978640ddb82440a99b9
Zanetta-15 <zcrentsil@gmail.com>
2023-09-25 11:39:35 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-/** 
- * main - This is the main part of the function
+/**
+ * main - this is the main put function
  * Return: 0
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
 
