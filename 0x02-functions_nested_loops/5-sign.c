d08550dceb5fcb3989489a0bdb302ff8cea42f4d
Zanetta-15 <zcrentsil@gmail.com>
2023-09-26 08:43:56 -0700

    5-main.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,27 +6,20 @@
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
- * -2 if none of the conditions are met
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
-
-    /* Handle the case when none of the conditions are met. */
-    return (-2); // You can choose an appropriate value here.
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
 }
-
+		_putchar(48);
+		return (0);
+	} else if (n < 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
