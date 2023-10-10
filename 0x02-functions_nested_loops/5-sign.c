341c78551858d17a533b86b73592bea62bab7bba
Zanetta-15 <zcrentsil@gmail.com>
2023-10-10 09:36:21 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,24 +10,24 @@
  */
 int print_sign(int n)
 {
-    if (n > 0)
-    {
-        _putchar('+');
-        return (1);
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return (0);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-        return (-1);
-    }
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
 
-    /* Handle other cases here if needed */
+	/* Handle other cases here if needed */
 
-    /* Default return statement in case none of the conditions are met */
-    return (0);
+	/* Default return statement in case none of the conditions are met */
+	return (0);
 }
